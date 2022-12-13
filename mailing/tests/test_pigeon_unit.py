01552466c0a355e67f448cf7e7326639b7f9c109
Evgeny Namsaraev <enamsaraev13@icloud.com>
2022-12-13 23:39:59 +0300

    test payment and pigeon tests

--- mailing/tests/test_pigeon_unit.py
+++ mailing/tests/test_pigeon_unit.py
@@ -5,11 +5,30 @@ from mixer.backend.django import mixer
 from django.core.mail import send_mail
 
 from orders.models import Order
+
 from mailing.pigeon import Pigeon
+from mailing.models import EmailEntry
 
 
 pytestmark = [pytest.mark.django_db]
 
+
+@pytest.fixture
+def return_order_for_mail():
+    """"""
+
+    return mixer.blend(Order)
+
+@pytest.fixture
+def return_sent_mail(return_order_for_mail):
+    """Set an alredy sent mail"""
+
+    return mixer.blend(
+        EmailEntry, 
+        to='fcknuuu@mail.com',
+        message='message',
+        order=return_order_for_mail,
+    )
 
 @pytest.mark.parametrize(('to, message, subject, create_order'), [ 
     ('fcknu@mail.com', 'message', 'subject', False),
@@ -32,16 +51,17 @@ def test_msg_params(to, message, subject, create_order):
     assert pigeon.order == order
 
 
-# def test_return_bad_if_mail_is_already_sent():
-#     """Assert fAlse if mail is already sent"""
+def test_return_bad_if_mail_is_already_sent(return_order_for_mail, return_sent_mail):
+    """Assert fAlse if mail is already sent"""
 
-#     Pigeon(
-#         to='fcknu@mail.com', 
-#         message='message', 
-#         subject='subject', 
-#         order=None
-#     )()
+    pigeon = Pigeon(
+        to='fcknuuu@mail.com', 
+        message='message', 
+        subject='subject',
+        order=return_order_for_mail,
+    )()
 
-#     assert len(send_mail.outbox) == 1
+    assert pigeon == False
+
 
 
