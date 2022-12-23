d5578d987e3ed0b0fafe850e97f8016511e0b8d4
enamsaraev <86794710+enamsaraev@users.noreply.github.com>
2022-12-23 21:45:40 +0300

    coupon-tests (#27)

--- ecommerce_api/tests/test_ecommerce_api.py
+++ ecommerce_api/tests/test_ecommerce_api.py
@@ -1,5 +1,7 @@
 import pytest
 
+from mixer.backend.django import mixer
+
 from django.urls import reverse
 
 
@@ -27,4 +29,14 @@ def test_retrieving_a_products_by_category_list(db, client, product_factory, pro
     response = client.get(reverse('ecommerce:products', kwargs={'slug': 'fckn'}))
 
     assert len(response.data) == 5
+
+
+@pytest.mark.ecommerce
+def test_product_cart_retrieving_images(db, client, product_factory, product_inventory_factory):
+    """Test on retrieving an img"""
+
+    prd = product_inventory_factory.create(product=product_factory.create(slug='fckn'), slug='aboba')
+    response = client.get(reverse('ecommerce:product_card', kwargs={'slug': 'fckn', 'product_slug': 'aboba'}))
+    print(response.data)
+    assert 'medias' in response.data[0]
 
