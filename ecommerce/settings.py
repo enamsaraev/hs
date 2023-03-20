02ae9044bb247a77b7e1df0b62d70a4fba9eeaaa
Evgeny Namsaraev <enamsaraev13@icloud.com>
2023-03-20 23:23:36 +0300

    pp

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = int(os.environ.get("DEBUG", default=0))
 
 # ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS")
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS")
+ALLOWED_HOSTS = [os.environ.get("DJANGO_ALLOWED_HOSTS"),]
 
 INTERNAL_IPS = [
     os.environ.get("INTERNAL_IPS")
