8fae3041296b1757dbd62b2ffb19a3158eedda1b
Andryschenko-George <andr.gosh9990@gmail.com>
2023-04-17 20:28:42 +0300

    testcdek

--- frontend/src/components/Checkout.vue
+++ frontend/src/components/Checkout.vue
@@ -7,32 +7,61 @@
 					<p class="subtitle">Контактные данные</p>
 				</div>
 				<div class="col-lg-7 in">
-					<input placeholder="ФИО" v-model="purch_info.name" class="col-lg-10" />
+					<input
+						placeholder="ФИО"
+						v-model="purch_info.name"
+						class="col-lg-10"
+					/>
 				</div>
 				<div class="col-lg-7 in">
-					<input placeholder="Контактный телефон" v-model.number="purch_info.phone" class="col-lg-10" />
+					<input
+						placeholder="Контактный телефон"
+						v-model.number="purch_info.phone"
+						class="col-lg-10"
+					/>
 				</div>
 				<div class="col-lg-7 in">
-					<input placeholder="Email" v-model="purch_info.email" class="col-lg-10" />
+					<input
+						placeholder="Email"
+						v-model="purch_info.email"
+						class="col-lg-10"
+					/>
 				</div>
 				<div class="col-lg-7 txt"><p class="subtitle">Доставка</p></div>
 				<div class="col-lg-6">
-					<input placeholder="Email" v-model.lazy="" class="col-lg-10" />
-					<div class="" >
-						<select>
-							<option value="0"></option>
+					<input
+						placeholder="Введите Ваш город"
+						v-model.lazy="city.to_location"
+						@change="chekSity()"
+						class="col-lg-10"
+					/>
+					<div v-if="adderssesfromcdek" class="">
+						<select v-modal="addressName">
+							<option disabled value="">Выберите один из пунктов выдачи</option>
+							<option v-for="adderssesincity in adderssesfromcdek" v-bind:value="adderssesincity" :key="adderssesincity">
+								{{ adderssesincity }}
+							</option>
 						</select>
+					</div>
+					<div class="" v-else>
+						{{ cdeknocity }}
 					</div>
 				</div>
 			</div>
 			<div class="col-lg-12 body_footer">
-				<router-link :to="{path: 'cart'}"><button class="col-lg-1 btn_brdr btn-p w-100" ><p>Назад</p></button></router-link>
-				<button 
-				type="button"
-				class="col-lg-4 btn btn_black btn-p"
-				:class="{disabled: activeBtn}" 
-				@click="order()"
-				><p>Подтвердить заказ</p></button>
+				<router-link :to="{path: 'cart'}"
+					><button class="col-lg-1 btn_brdr btn-p w-100">
+						<p>Назад</p>
+					</button></router-link
+				>
+				<button
+					type="button"
+					class="col-lg-4 btn btn_black btn-p"
+					:class="{disabled: activeBtn}"
+					@click="order()"
+				>
+					<p>Подтвердить заказ</p>
+				</button>
 			</div>
 		</div>
 	</div>
@@ -40,9 +69,8 @@
 
 <script>
 import axios from "axios";
-import { mapGetters } from "vuex";
+import {mapGetters} from "vuex";
 import router from "@/router";
-
 
 export default {
 	data() {
@@ -51,43 +79,95 @@ export default {
 				name: "",
 				email: "",
 				phone: "",
-				address: "",/* Тут должен быть город и адресс пункта выдачи */
-				delivery_price: "",/* Цена от сдека */
-				total_price: "0"
+				address: "" /* Тут должен быть город и адресс пункта выдачи */,
+				delivery_price: "" /* Цена от сдека */,
+				total_price: "0",
 			},
+			city: {
+				to_location: ""
+			},
+			addressName: "",
+			adderssesfromcdek: {},
+			cdekreq: {},
+			cdeknocity:"",
 		};
 	},
 	computed: {
 		...mapGetters(["getAllCart"]),
 		activeBtn() {
-			if (this.purch_info.name == "" || this.purch_info.email == "" || this.purch_info.phone == "" || this.purch_info.delivery_price == "") {
-				return true
+			if (
+				this.purch_info.name == "" ||
+				this.purch_info.email == "" ||
+				this.purch_info.phone == "" ||
+				this.purch_info.delivery_price == "" ||
+				this.addressName == ""
+			) {
+				return true;
 			}
-			return false
-		}
+			return false;
+		},
 	},
-	methods:{
+	methods: {
+		chekSity() {
+			/* headers */
+			let csrf = document.cookie.split("=");
+			let headers = {
+				"Content-Type": "application/json;charset=utf-8",
+				"X-CSRFToken": `${csrf[1]}`,
+			};
+			if (localStorage.getItem("HTTP_TOKEN") !== "") {
+				headers["TOKEN"] = localStorage.getItem("HTTP_TOKEN");
+			}
+			
+			axios
+				.post(
+					`${this.$store.state.BaseUrl}api/cdek/prices/`,
+					this.city,
+					{headers},
+				)
+				.then((response) => ( this.cdekreq = response.data))
+				.catch((error) => console.log(error));
+
+			if (cdekreq.result == "Такого города нет") {
+				this.cdeknocity = cdekreq.result;
+				}
+			else {
+				this.adderssesfromcdek = cdekreq.result.addresses
+			}
+		},
+
 		order() {
-			this.purch_info.total_price = (parseInt(this.getAllCart.data.total * 100)) / 100;
+			this.purch_info.total_price =
+				parseInt(this.getAllCart.data.total * 100) / 100;
 
 			/* headers */
-			let csrf = document.cookie.split('=');
-			let headers = {"Content-Type": "application/json;charset=utf-8","X-CSRFToken": `${csrf[1]}`};
+			let csrf = document.cookie.split("=");
+			let headers = {
+				"Content-Type": "application/json;charset=utf-8",
+				"X-CSRFToken": `${csrf[1]}`,
+			};
 			if (localStorage.getItem("HTTP_TOKEN") !== "") {
 				headers["TOKEN"] = localStorage.getItem("HTTP_TOKEN");
 			}
 
-
 			if (this.getAllCart.data.discount.code != null) {
-				this.purch_info.code=`${this.getAllCart.data.discount.code}`
+				this.purch_info.code = `${this.getAllCart.data.discount.code}`;
+			}
+			if (this.addressName != null) {
+				this.purch_info.address = `${this.addressName}, ${this.city.to_location}`;
+				this.purch_info.delivery_price = this.cdekreq.result.amount.total_sum;
+				this.purch_info.total_price = parseInt((this.getAllCart.data.total + this.cdekreq.result.amount.total_sum)  * 100) / 100;
 			}
 			axios
-				.post(`${this.$store.state.BaseUrl}api/orders/create_order/`, this.purch_info, {headers})
-				.then((response) => window.location.href = `${response.data.url}`)
+				.post(
+					`${this.$store.state.BaseUrl}api/orders/create_order/`,
+					this.purch_info,
+					{headers},
+				)
+				.then((response) => (window.location.href = `${response.data.url}`))
 				.catch((error) => console.log(error));
-				
 		},
-	}
+	},
 };
 </script>
 
@@ -99,7 +179,7 @@ p {
 	margin-inline-start: 0px;
 	margin-inline-end: 0px;
 }
-.container{
+.container {
 	height: 100vh;
 }
 
@@ -143,19 +223,19 @@ p {
 	justify-content: center;
 	border: 2px solid black;
 }
-.body_footer{
+.body_footer {
 	display: flex;
 	justify-content: space-between;
 	padding: 0px;
 	margin-top: 50px;
 }
-.btn_brdr{
+.btn_brdr {
 	border: 1px solid black;
-	background-color: #FFF;
+	background-color: #fff;
 }
-.btn_black{
+.btn_black {
 	background-color: #000;
-	color: #FFF;
+	color: #fff;
 }
 .in {
 	display: flex;
@@ -164,22 +244,22 @@ p {
 	margin-bottom: 15px;
 }
 
-button p{
+button p {
 	margin: 0;
 }
 
-.btn-p{
+.btn-p {
 	padding: 10px;
 }
 
-.btn.disabled{
+.btn.disabled {
 	opacity: 1;
 	color: white;
-    background-color: #49484a;
+	background-color: #49484a;
 }
 .btn:hover {
-    background-color: #000;
-	color: #FFF;
+	background-color: #000;
+	color: #fff;
 }
 
 @media (min-width: 1400px) {
