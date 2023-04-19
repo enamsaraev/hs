fccac8086ae5b0fbc23dc5c0fce179f051dc8138
Evgeny Namsaraev <enamsaraev13@icloud.com>
2023-04-19 21:50:41 +0300

    delete post method

--- backend/cart/urls.py
+++ backend/cart/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from cart.cart_views import CartApiView
+from cart.cart_views import CartApiView, DeleteCartData
 
 
 app_name = 'cart'
@@ -9,5 +9,5 @@ app_name = 'cart'
 urlpatterns = [
     path('', CartApiView.as_view(), name='return_cart'),
     path('add/', CartApiView.as_view(), name='add_or_update_cart'),
-    path('delete/<slug:slug>/', CartApiView.as_view(), name='delete_cart_product'),
+    path('delete/', DeleteCartData.as_view(), name='delete_cart_product'),
 ]
