11cc785cdc58b73a38f6dcd27844f72290214b48
enamsaraev <86794710+enamsaraev@users.noreply.github.com>
2023-05-01 00:07:58 +0300

    as pay (#87)

--- backend/payment/tasks.py
+++ backend/payment/tasks.py
@@ -15,22 +15,16 @@ from payment.models import PaymentData
 @shared_task
 def check_payments_status(payment_id: str, order_id: int, cart: dict):
     """Checking a paymnet status"""
+    order = Order.objects.get(id=order_id)
+    order.set_is_paid()
 
-    time.sleep(30)
+    OrderSetCount(
+        cart=cart,
+        order_id=order_id
+    )()
 
-    payment = json.loads((Payment.find_one(payment_id)).json())
-
-    if payment['status']=='succeeded':
-        order = Order.objects.get(id=order_id)
-        order.set_is_paid()
-
-        OrderSetCount(
-            cart=cart,
-            order_id=order_id
-        )()
-
-        PaymentData.objects.create(
-            payment_id=payment_id,
-            order=order
-        )
+    PaymentData.objects.create(
+        payment_id=payment_id,
+        order=order
+    )
         
