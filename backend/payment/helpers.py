35d928168602a3a4d0945a51f3789354586d4db6
enamsaraev <86794710+enamsaraev@users.noreply.github.com>
2023-05-01 00:21:53 +0300

    as f (#88)

--- backend/payment/helpers.py
+++ backend/payment/helpers.py
@@ -1,36 +1,9 @@
-import asyncio
-import json
 from payment.yk import create_payment
+from payment.tasks import check_payments_status
 from mailing.tasks import send_mail
-from orders.models import Order
-from orders.order_components import OrderComponent, OrderSetCount
-
-from payment.models import PaymentData
-from yookassa import Payment
 
 
-async def check_payments_status(payment_id: str, order_id: int, cart: dict):
-    payment = json.loads((Payment.find_one(payment_id)).json())
-
-    while payment['status'] == 'pending':
-        payment = json.loads((Payment.find_one(payment_id)).json())
-        await asyncio.sleep(3)
-
-    if payment['status'] == 'succeeded':
-        order = Order.objects.get(id=order_id)
-        order.set_is_paid()
-
-        OrderSetCount(
-            cart=cart,
-            order_id=order_id
-        )()
-
-        PaymentData.objects.create(
-            payment_id=payment_id,
-            order=order
-        )
-        
-async def get_create_payment(
+def get_create_payment(
         price: str, description: str, order_id: int, cart: dict
     ) -> dict:
     
--- backend/payment/tasks.py
+++ backend/payment/tasks.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import, unicode_literals
 
 import json
-import time
+import asyncio
 
 from yookassa import Payment
 from celery import shared_task
@@ -12,19 +12,25 @@ from orders.order_components import OrderComponent, OrderSetCount
 from payment.models import PaymentData
 
 
-@shared_task
-def check_payments_status(payment_id: str, order_id: int, cart: dict):
+async def check_payments_status(payment_id: str, order_id: int, cart: dict):
     """Checking a paymnet status"""
-    order = Order.objects.get(id=order_id)
-    order.set_is_paid()
 
-    OrderSetCount(
-        cart=cart,
-        order_id=order_id
-    )()
+    payment = json.loads((Payment.find_one(payment_id)).json())
+    while payment['status'] == 'pending':
+        payment = json.loads((Payment.find_one(payment_id)).json())
+        await asyncio.sleep(3)
 
-    PaymentData.objects.create(
-        payment_id=payment_id,
-        order=order
-    )
+    if payment['status']=='succeeded':
+        order = Order.objects.get(id=order_id)
+        order.set_is_paid()
+
+        OrderSetCount(
+            cart=cart,
+            order_id=order_id
+        )()
+
+        PaymentData.objects.create(
+            payment_id=payment_id,
+            order=order
+        )
         
