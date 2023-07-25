b31bb146cdfc429d4f925101c9442340bb119b96
enamsaraev <86794710+enamsaraev@users.noreply.github.com>
2023-07-25 17:18:51 +0300

    dbmodel correction and admin (#92)

--- backend/payment/admin.py
+++ backend/payment/admin.py
@@ -5,8 +5,12 @@ from payment.models import PaymentData
 
 @admin.register(PaymentData)
 class PaymentDataAdmin(admin.ModelAdmin):
-    list_display = ('order', 'is_paid')
+    list_display = ('order', 'is_paid', 'order_is_paid')
     readonly_fields = ('payment_id', 'order', 'is_paid')
+
+    @admin.display(ordering='order__is_paid', description='Заказ оплачен')
+    def order_is_paid(self, obj):
+        return 'Да' if obj.order.is_paid else 'Нет'
 
     def has_delete_permission(self, request, obj=None):
         return False
